72f7cd9db46af390066fa3123025e9779afd92b4
Ben Williams <DKBF78@motorolasolutions.com>
2026-08-13 12:49:32 -0400

    removed player fields from EntityState class

--- Assets/Scripts/EntityState.cs
+++ Assets/Scripts/EntityState.cs
@@ -3,37 +3,31 @@ using UnityEngine;
 
 public abstract class EntityState 
 {
-    protected Unity.VisualScripting.StateMachine stateMachine;
     protected string stateName;
-    private StateController stateMachine1;
+    protected StateController controller;
     private string v;
 
-    public  EntityState(Unity.VisualScripting.StateMachine sm, string _name)
-    {
-        stateMachine = sm;
-        stateName = _name;
-    }
 
-    public EntityState(StateController controller, string v)
+    public EntityState(StateController _controller, string v)
     {
-        this.stateMachine1 = controller;
+        this.controller = _controller;
         this.v = v;
     }
 
     // Called when the state is entered
     public virtual void Enter() 
     {
-
+        Debug.Log($"Entering state: {v}");
     }
     // Called when the state is exited
     public virtual void Exit() 
     {
-
+        Debug.Log($"Exiting state: {v}");
     }
 
     // Called every frame the state is active
     public virtual void Update() 
     {
-        
+        Debug.Log($"Updating state: {v}");
     }
 }
