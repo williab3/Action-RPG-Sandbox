f9e3d487a17ed43f6d2203cf92dc882d2c035248
Ben Williams <DKBF78@motorolasolutions.com>
2026-08-14 04:16:59 -0400

    renamed variable for capturing transition parameter in EntityState class

--- Assets/Scripts/EntityState.cs
+++ Assets/Scripts/EntityState.cs
@@ -5,29 +5,29 @@ public abstract class EntityState
 {
     protected string stateName;
     protected StateController controller;
-    private string v;
+    protected string animXParam;
 
 
     public EntityState(StateController _controller, string v)
     {
         this.controller = _controller;
-        this.v = v;
+        this.animXParam = v;
     }
 
     // Called when the state is entered
     public virtual void Enter() 
     {
-        Debug.Log($"Entering state: {v}");
+        Debug.Log($"Entering state: {animXParam}");
     }
     // Called when the state is exited
     public virtual void Exit() 
     {
-        Debug.Log($"Exiting state: {v}");
+        Debug.Log($"Exiting state: {animXParam}");
     }
 
     // Called every frame the state is active
     public virtual void Update() 
     {
-        Debug.Log($"Updating state: {v}");
+        Debug.Log($"Updating state: {animXParam}");
     }
 }
